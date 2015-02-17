34cb4d742f76f35df2e99901bc228f1dfa165198
Pavel Sokolov <pavel.v.sokolov@gmail.com>
2015-02-17 17:16:46 +0300

    Chapter 4 update

---
+++ code5/js/Stage.js
@@ -0,0 +1,59 @@
+(function(w, TGE) {
+
+    /**
+     * Game stage basic class
+     * @memberOf TiledGameEngine
+     * @param {string}      name    Stage name
+     * @constructor
+     */
+    function Stage(name) {
+        /** @type {boolean}         Indicates if the stage is active */
+        this.active = false;
+        
+        /** @type {string}          Store stage name */
+        this.name = name;
+        
+        /** @type {boolean}         Indicates if the stage need to be redrawn */
+        this.redraw = false;
+    };
+    
+    Stage.prototype = {
+        /**
+         * Update stage internal objects. That routine called by main game loop
+         * @param {number}  dt      time difference from last update
+         * @return {number}         Return time difference (in ms) to next update
+         */
+        update: function(dt) {
+            if (this.active) {
+                return 16; // 60 FPS by default
+            } else {
+                return 1000; // 1 FPS in inactive state
+            }
+        },
+        
+        /**
+         * Render the stage to the viewport. That routine called by main game loop
+         */
+        render: function() {
+            if (!this.active) return;
+            
+            // your render code goes here
+        },
+        
+        /**
+         * Activate stage and prepare for rendering
+         */
+        deactivate: function() {
+            this.active = false;
+        },
+        
+        /**
+         * Deactivate stage
+         */
+        activate: function() {
+            this.active = true;
+        }
+    };
+    
+    TGE['Stage'] = Stage;
+})(window, TiledGameEngine);
---
+++ code5/js/game.js
@@ -0,0 +1,51 @@
+// @constructor
+function Game() {
+    // call super-class constructor
+    TiledGameEngine.call(this);
+    
+    // create canvas in the #viewport container
+    this.screen = new TiledGameEngine.Screen('#viewport', 320, 200);
+
+    // set "world" background
+    this.screen.setBGColor('gray');
+    
+    // creat assets manager instance
+    this.assets = new TiledGameEngine.Assets();
+    
+    // create LoadingStage instance
+    this.stLoader = new TiledGameEngine.LoadingStage(this.assets, true, this.screen);
+};
+
+Game.prototype = (function() {
+    // super-class inheritance
+    var o = Object.create(TiledGameEngine.prototype);
+    
+    o.init = function() {
+        // call super-class
+        TiledGameEngine.prototype.init.call(this);
+
+        // add loader stage into game loop manager
+        this.addStage('load', this.stLoader);
+        
+        // request assets from server
+        this.stLoader.request(['sage_home.png', 'sage_home.json', 'grassland1.json']);
+
+        // activate loader stage. Loading will be started automatically (see stLoader creation)
+        this.activateStage('load');
+
+        // show screen
+        this.screen.show(true);
+    };
+    
+    return o;
+})();
+
+function start() {
+    var G = new Game();
+    
+    G.init();
+    
+    G.start();
+};
+
+window.onload = start;
